c704967a7f136ce6b000620c55f068ce3607210f
Eduardo Minguez <e.minguez@gmail.com>
2022-09-27 13:14:14 +0200

    First try

---
+++ sysdig-to-defectdojo.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import json
+import os
+import sys
+import argparse
+import re
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--infile', '-i', type=argparse.FileType('r', encoding='UTF-8'), 
+                      help="Input file", required=True)
+  parser.add_argument('--outfile', '-o', help="Output file", required=True)
+  parser.add_argument('--title', '-t', default="Sysdig vulnerability report",
+                      help="Report title (default: Sysdig vulnerability report).",
+                      required=False)
+  args = parser.parse_args()
+  return args
+
+def main():
+  args=parse_args()
+  try:
+    with open(args.infile.name) as f:
+      sourcejson = json.load(f)
+  except ValueError as e:
+    print('invalid json: %s' % args.infile.name)
+    return None
+
+  outfile = args.outfile
+  reportdate = os.path.basename(args.infile.name).split("-")[0]
+  reportdate = reportdate[:4] + '-' + reportdate[4:6] + '-' + reportdate[6:]
+  destjson = {}
+  destjson["title"] = args.title
+  destjson["findings"] = []
+  
+  
+  for dic in sourcejson['data']:
+    description = f"""packageName: {dic["packageName"]}
+                      packageType: {dic["packageType"]}
+                      packagePath: {dic["packagePath"]}
+                      packageVersion: {dic["packageVersion"]}
+                      imagePullString: {dic["imagePullString"]}
+                      imageId: {dic["imageId"]}
+                      osName: {dic["osName"]}
+                      k8sClusterName: {dic["k8sClusterName"]}
+                      k8sNamespaceName: {dic["k8sNamespaceName"]}
+                      k8sWorkloadType: {dic["k8sWorkloadType"]}
+                      k8sWorkloadName: {dic["k8sWorkloadName"]}
+                      k8sPodContainerName: {dic["k8sPodContainerName"]}"""
+    destjson["findings"].append({
+      "date": reportdate,
+      "title": dic["vulnName"],
+      "cve": dic["vulnName"],
+      "severity": dic["vulnSeverity"],
+      "description": re.sub(' +', ' ', description),
+      "references": dic["vulnLink"]
+    })
+
+  with open(outfile, "w") as outfile:
+    outfile.write(json.dumps(destjson))
+
+if __name__ == "__main__":
+    main()
